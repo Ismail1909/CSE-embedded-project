98fa002fa206146cb8c2ed5cde3adeed29face9d
Ismail1909 <68963264+Ismail1909@users.noreply.github.com>
2021-06-03 17:14:28 +0200

    Update gps.c

--- gps.c
+++ gps.c
@@ -53,19 +53,5 @@ void lcd_data(char data);
 void lcd(void);
 long calculate_distance( long double lat1, long double lon1, long double lat2, long double lon2);
 
-void PortF_Init(){
-	 SYSCTL_RCGCGPIO_R |= 0x00000020;     // 1) F clock
-   while((SYSCTL_PRGPIO_R & 0x20) == 0);         
-  GPIO_PORTF_LOCK_R = 0x4C4F434B;   // 2) unlock PortF PF0  
-  GPIO_PORTF_CR_R |= 0x01;           // allow changes to PF4-0       
-  GPIO_PORTF_AMSEL_R &= ~0x1F;        // 3) disable analog function
-  GPIO_PORTF_PCTL_R &= ~0x000FFFFF;   // 4) GPIO clear bit PCTL  
-  GPIO_PORTF_DIR_R = 0x0E;          // 5) PF4,PF0 input, PF3,PF2,PF1 output   
-  GPIO_PORTF_AFSEL_R &= ~0x1F;        // 6) no alternate function
-  GPIO_PORTF_PUR_R |= 0x11;          // enable pullup resistors on PF4,PF0       
-  GPIO_PORTF_DEN_R |= 0x1F;          // 7) enable digital pins PF4-PF0
-}
-
-
 
 
